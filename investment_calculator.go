6436276457b6c9e6a3ef82ad1530f53b89233531
Burkin Dmytro <deluxing1234@gmail.com>
2024-10-06 13:33:39 +0300

    13

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,26 +5,37 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 6.5
+
 	var investmentAmount float64
 	var years float64
-	var expectedReturnRate float64
+	expectedReturnRate := 5.5
 
-	fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Years: ")
+	outputText("Years: ")
 	fmt.Scan(&years)
 
-	fmt.Print("Expected Return Rate: ")
+	outputText("Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
 	formatedFV := fmt.Sprintf("Future value: %.4f \n", futureValue)
 	fmt.Print(formatedFV)
 	formatedFRV := fmt.Sprintf("Future real value: %.4f", futureRealValue)
 	fmt.Print(formatedFRV)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
